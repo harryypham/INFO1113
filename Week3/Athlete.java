694658ba3c37cf1fae6cb2d7b4468e1698bdda81
Harry Pham <83818093+harryypham@users.noreply.github.com>
2024-03-05 15:52:34 +1100

    Week 3 - Question 2

---
+++ Week3/Athlete.java
@@ -0,0 +1,27 @@
+//Week 3 - Question 2
+public class Athlete {
+    public String name;
+    public double time;
+
+    public Athlete(String name, double time) {
+        this.name = name;
+        this.time = time;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getTime() {
+        return this.time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+}
